061b5db2bf5e8ede9b52bf8bb60bf2c4107bf677
egor <7877770@gmail.com>
2022-08-04 12:52:07 +0300

    FIX: locators fixed

--- pages/locators.py
+++ pages/locators.py
@@ -9,3 +9,7 @@ class LoginPageLocators:
     LOGIN_FROM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
 
+
+class ProductPageLocators:
+    ADD_TO_BASKET_BTN = (By.CSS_SELECTOR, ".btn-add-to-basket")
+
