ccab0e184af124094345118495ce3fbac2caca90
egor <7877770@gmail.com>
2022-08-04 17:32:48 +0300

    ADD: markers for coursemates

--- test_product_page.py
+++ test_product_page.py
@@ -9,6 +9,7 @@ import pytest
 #                          pytest.param("7", marks=pytest.mark.xfail),
 #                          *[8, 9]])
 class TestGuestProductPage:
+    @pytest.mark.need_review
     def test_guest_can_add_product_to_basket(self, browser, offer_number=0):
         link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{offer_number}"
         page = ProductPage(browser, link)
@@ -47,12 +48,14 @@ class TestGuestProductPage:
         page.open()
         page.should_be_login_link()
 
+    @pytest.mark.need_review
     def test_guest_can_go_to_login_page_from_product_page(self, browser):
         link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
         page = ProductPage(browser, link)
         page.open()
         page.go_to_login_page()
 
+    @pytest.mark.need_review
     def test_guest_cant_see_product_in_basket_opened_from_product_page(self, browser):
         link = "http://selenium1py.pythonanywhere.com/"
         page = ProductPage(browser, link)
@@ -76,6 +79,7 @@ class TestUserAddToBasketFromProductPage:
         login_page.register_new_user(email, "do948y9y49ymd94y9-34y")
         login_page.should_be_authorized_user()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
         page = ProductPage(browser, link)
